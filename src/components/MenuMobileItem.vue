e9e450279d6cd637c4b87f664002f2144256302c
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 20:06:43 +0100

    make MenuMobileItem component for reuse

---
+++ src/components/MenuMobileItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>
+    <li>
+      <router-link :to="to">
+        <slot />
+      </router-link>
+    </li>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MenuMobileItem",
+  props: {
+    to: String,
+  },
+};
+</script>

360c0bb809fa18d70e85a9c51f421c385a692463
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 14:52:24 +0100

    made reusable Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button
+      class="border-white border-2 p-3 transform -skew-y-6 transition duration-300 hover:scale-110 hover:text-transparent hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500 hover:bg-clip-text"
+    >
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>

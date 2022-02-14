0bab4a608307f8ed4717b8940906645e0a82eed8
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 18:17:25 +0100

    make my project component and add title for it also add style

---
+++ src/pages/Projects.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="px-6">
+    <h2 class="text-3xl">
+      <span
+        class="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-pink-600"
+        >My</span
+      >
+      projects
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+};
+</script>

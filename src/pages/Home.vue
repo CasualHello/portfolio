4ca2be29b9144fc22d4b4a9d0cc2d07783b7918b
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 14:08:46 +0100

    add text and style for Home component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>asd asdada</div>
+  <div class="text-white">
+    <p>
+      Hey, my name is
+      <span
+        class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-yellow-500"
+        >Maria Halkiewicz</span
+      >. I’m freelancer how makes websites.
+    </p>
+  </div>
 </template>
 
 <script>

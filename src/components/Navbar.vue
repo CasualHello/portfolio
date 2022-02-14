6ba5b913a882f5df64dc0894ab6bf4721892ebca
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 13:10:51 +0100

    make and add style Navbar with my logo and icon

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="border-white border-t-4 border-b-4 px-2">
+    <img src="@/assets/images/logo.svg" alt="logo" class="py-4" />
+    <button>
+      <img src="@assets/images/menu-icon.svg" alt="menu icon" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>

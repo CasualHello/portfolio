3fce279d56b3b0f2f4f6bb86efc71558b24000d1
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 18:14:56 +0100

    add path for projects components

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
+import Projects from "../pages/Projects.vue";
 
 const routes = [
   {
@@ -11,15 +12,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  // {
-  // path: "/about",
-  // name: "About",
-  // // route level code-splitting
-  // // this generates a separate chunk (about.[hash].js) for this route
-  // // which is lazy-loaded when the route is visited.
-  // component: () =>
-  //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
+  {
+    path: "/my-projects",
+    name: "Projects",
+    component: Projects,
+  },
 ];
 
 const router = createRouter({

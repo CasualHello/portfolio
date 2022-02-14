e5a170f9e4e22d130f6dee6194a11107d8bc87f8
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 14:54:52 +0100

    add Button component, add style for Home component, add images for Home component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,52 @@
 <template>
-  <div class="text-white">
-    <p>
+  <div class="text-white px-6">
+    <p class="text-3xl tracking-wider leading-10 mb-10">
       Hey, my name is
       <span
-        class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-yellow-500"
+        class="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-pink-600"
         >Maria Halkiewicz</span
       >. I’m freelancer how makes websites.
     </p>
+    <Button>My projects </Button>
+
+    <div class="flex justify-center">
+      <img
+        src="@/assets/images/neon-balls-blue.svg"
+        alt="neon blue and green balls"
+        class="w-40 rotate"
+      />
+
+      <img
+        src="@/assets/images/neon-balls-pink.svg"
+        alt="neon pink and purple balls"
+        class="w-48 rotate"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Button from "@/components/Button.vue";
+
 export default {
   name: "Home",
+  components: {
+    Button,
+  },
 };
 </script>
+
+<style scoped>
+.rotate {
+  animation: rotateAnimation 20s linear infinite;
+}
+
+@keyframes rotateAnimation {
+  from {
+    transform: rotateZ(0deg);
+  }
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+</style>

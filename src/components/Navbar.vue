7cf73b39fae05857816e88e2eaf88b3c93c84206
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 13:11:35 +0100

    add style to Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="border-white border-t-4 border-b-4 px-2">
-    <img src="@/assets/images/logo.svg" alt="logo" class="py-4" />
-    <button>
-      <img src="@assets/images/menu-icon.svg" alt="menu icon" />
+  <div class="border-white border-t-4 border-b-4 px-2 flex">
+    <img
+      src="@/assets/images/logo.svg"
+      alt="logo"
+      class="py-4 cursor-pointer"
+    />
+    <button class="ml-auto">
+      <img src="@/assets/images/menu-icon.svg" alt="menu icon" />
     </button>
   </div>
 </template>

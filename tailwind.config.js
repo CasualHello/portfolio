8c8ae007a11b99d03b9485140483532e0cc4c447
CasualHello <86096604+CasualHello@users.noreply.github.com>
2022-02-14 20:03:51 +0100

    adjust tailwind.config for bg-clip and bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundClip: ["hover"],
+      backgroundImage: ["hover"],
+    },
   },
   plugins: [],
 };
